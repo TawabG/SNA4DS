36d9e4c01e23180f2d31dcf4e6bdb83c9ec022cf
dylanvanw <dylanvanwonderen@gmail.com>
2021-10-15 13:27:44 +0200

    reading functions

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # SNA4DS
 SNA4DS team 14
 
+
+# Data
+Get the data from here: https://www.kaggle.com/netflix-inc/netflix-prize-data
+This is in the gitignore so the repository will not be too big
+
 # Data exploration and Slicing
 The exploration that is used to decide how much of the data should be sliced away and how it is done is taken from this kernel: https://www.kaggle.com/guyhaimovitz/kernel1de78d7269
---
+++ data_slicing.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+
+def load_ratings(files):
+    ratings_df = pd.DataFrame(columns=['CustomerID','Rating', 'Date'])
+    for file_location in files:
+        partial_ratings = pd.read_csv(file_location, header = None, names = ['CustomerID','Rating', 'Date'], usecols = [0,1, 2])
+        partial_ratings['Rating'] = partial_ratings['Rating'].astype(float)
+        ratings_df = ratings_df.append(partial_ratings)
+    return ratings_df
+
+def map_user_ratings_to_movies(ratings_df):
+    df_nan = pd.DataFrame(pd.isnull(ratings_df.Rating))
+    df_nan = df_nan[df_nan['Rating'] == True]
+    df_nan = df_nan.reset_index()
+
+    movie_np = []
+    movie_id = 1
+
+    for i,j in zip(df_nan['index'][1:],df_nan['index'][:-1]):
+
+        temp = np.full((1,i-j-1), movie_id)
+        movie_np = np.append(movie_np, temp)
+        movie_id += 1
+
+    last_record = np.full((1,len(ratings_df) - df_nan.iloc[-1, 0] - 1),movie_id)
+    movie_np = np.append(movie_np, last_record)
+
+    df = ratings_df[pd.notnull(ratings_df['Rating'])]
+
+    df['Movie_Id'] = movie_np.astype(int)
+    df['CustomerID'] = df['CustomerID'].astype(int)
+    return df
+
+
+
+def trim_ratings_data_by_percentile(df, movies_percentile=0.7, users_percentile=0.7):
+    df_movie_summary = df.groupby('Movie_Id')['Rating'].agg(['count'])
+    df_movie_summary.index = df_movie_summary.index.map(int)
+    movie_benchmark = round(df_movie_summary['count'].quantile(movies_percentile),0)
+    drop_movie_list = df_movie_summary[df_movie_summary['count'] < movie_benchmark].index
+
+    df_cust_summary = df.groupby('CustomerID')['Rating'].agg(['count'])
+    df_cust_summary.index = df_cust_summary.index.map(int)
+    cust_benchmark = round(df_cust_summary['count'].quantile(users_percentile),0)
+    drop_cust_list = df_cust_summary[df_cust_summary['count'] < cust_benchmark].index
+
+    print('Movies minimum rating count: {}'.format(movie_benchmark))
+    print('Customers minimum rating count: {}'.format(cust_benchmark))
+
+    print('Original Shape: {}'.format(df.shape))
+    df = df[~df['Movie_Id'].isin(drop_movie_list)]
+    df = df[~df['CustomerID'].isin(drop_cust_list)]
+
+    print('After Trim Shape: {}'.format(df.shape))
+
+
+if __name__ == '__main__':
+    files = ['../combined_data_1.txt', '../combined_data_2.txt', '../combined_data_3.txt', '../combined_data_4.txt']
+    ratings_df = load_ratings(files)
+
+    print(ratings_df.head())
+
+
+
+    
